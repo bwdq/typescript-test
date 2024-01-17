a6a99cb6d5cea2fbe2741ce6923ff79101f5e9da
bwdq <louis.marquiss@seattlecolleges.edu>
2024-01-17 12:22:36 -0800

    typescript testing

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,25 @@
 var hello = "world";
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName("Monster", "Lessons"));
+var user = {
+    name: "Monster",
+    age: 30,
+    getMessage: function () {
+        return "Hello" + name;
+    }
+};
+var user2 = {
+    name: "Jack",
+    age: 30,
+    getMessage: function () {
+        return "Hello" + name;
+    }
+};
+console.log(user.name);
+var username = "alex";
+var pageName = "1";
+var errorMessage = null;
+var user3 = null;
+var someProp;
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,41 @@
 let hello: string = "world";
+
+const getFullName = (name: string, surname: string): string => {
+    return name + " " + surname;
+};
+
+console.log(getFullName("Monster", "Lessons"))
+
+const user: UserInterface = {
+    name: "Monster",
+    age: 30,
+    getMessage() {
+        return "Hello" + name;
+    }
+};
+
+const user2: UserInterface = {
+    name: "Jack",
+    age: 30,
+    getMessage() {
+        return "Hello" + name;
+    }
+};
+
+interface UserInterface {
+    name: string;
+    age?: number;
+    getMessage(): string,
+}
+
+console.log(user.name)
+
+let username = "alex";
+
+let pageName: string | number = "1";
+
+let errorMessage: string | null = null;
+
+let user3: UserInterface | null = null;
+
+let someProp: string | number | null | undefined | string[] | object;
